f0c45630ed1abe0cf18d520bcf7e8d9fcd9672ab
Ferdi Mehmedov <FerdiIt@pm.me>
2025-03-12 11:56:23 +0200

    create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('MVP API is running....');
+});
