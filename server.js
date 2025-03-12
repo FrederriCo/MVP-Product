9fc6ac453e2ae62513449e30fd968a0d1d073ada
Ferdi Mehmedov <FerdiIt@pm.me>
2025-03-12 16:48:31 +0200

    add port and router adText

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const adTextRouter = require('./routers/adText');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/adText', adTextRouter);
 
 app.get('/', (req, res) => {
   res.send('MVP API is running....');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
