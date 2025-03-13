e898fb3d9a62010e61f288cddb5b32786b5918fb
Ferdi Mehmedov <FerdiIt@pm.me>
2025-03-13 14:50:04 +0200

    openai config

--- openai.js
+++ openai.js
@@ -4,3 +4,7 @@ require('dotenv').config();
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
+
+const openai = new OpenAIApi(configuration);
+
+module.exports = openai;
